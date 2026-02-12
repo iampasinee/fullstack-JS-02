c8d90551ce863a458a730687ebf4ed91d8095682
iampasinee <pasinee.nong@gmail.com>
2026-02-12 15:14:59 +0700

    save

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -1,17 +1,20 @@
 
-
 const express = require('express');
 const axios = require('axios');
-const app = express();
 var bodyParser = require('body-parser');
+const path = require('path')
+const app = express();
 
 const base_url = "http://localhost:3000";
+//const base_url = "https://.ruk-com.cloud";
 
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/public/views"));
+app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + '/public'));
+// Serve static files
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", async (req, res) => {
     try {
